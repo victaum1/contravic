a4d4d4dabd6578f3bf8788370f747200d63f8bb1
Victor H. Manotas G <victorma31@gmail.com>
2019-09-12 19:00:44 -0500

    cli: testing encrypt fail

--- tests/test_contras_cli.py
+++ tests/test_contras_cli.py
@@ -2,7 +2,7 @@
 # testin main: contras_cli
 
 from unittest import TestCase, skip
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import sys
 
 sys.path.append("../src")
@@ -17,13 +17,22 @@ class TestMain(TestCase):
       ins = TestMain.inputs
       ots = TestMain.outputs
       ots.append(val)
-      if len(ots) != 0:
+      if len(ins) != 0:
         return ins.pop(0)
       return None
 
-#      def print_f(val=''):
-#        TestMain.outputs.append(val)
-#        return None
+    def print_f(val=''):
+      TestMain.outputs.append(val)
+      return None
+
+    def create_patch(self, name, obj=None):
+      if obj == None:
+        patcher = patch(name)
+      else:
+        patcher = patch.object(obj, name)
+      thing = patcher.start()
+      self.addCleanup(patcher.stop)
+      return thing
 
     @classmethod
     def setUpClass(self):
@@ -31,10 +40,6 @@ class TestMain(TestCase):
       self.outputs = TestMain.outputs
 
     def setUp(self):
-        pass
-
-    @skip("Not ready")
-    def test_input_file_found():
         pass
 
     @patch.object(cli,'input', side_effect = input_f)
@@ -50,12 +55,44 @@ class TestMain(TestCase):
         self.assertEqual(self.outputs, ['Input file name to process it: ',
             ''])
     
-    @skip("Not ready")
-    def test_encrypting_ok():
-        pass
+    @patch("builtins.input", side_effect = input_f)
+    @patch("builtins.print", side_effect = print_f)
+    def test_encrypting_ok(self, m_print, m_input):
+        infile   = "somefile.txt"
+        outfile  = "scrambled.txt"
+        key      = "some key!"
+        out_cont = "oooooooooooo"
+        in_cont  = "iiiiiiiiiiii"
+        i_f = Mock()
+        i_f.read.return_value = in_cont
+        o_f = Mock()
+        self.inputs.append(infile)
+        self.inputs.append("e")
+        m_open = self.create_patch("open", obj=cli)
+        m_open.side_efect = [i_f, o_f]
+        m_gpw = self.create_patch("gpw.getpass", obj=cli)
+        m_gpw.return_value = key
+        m_cpt = self.create_patch("cpt.encrypt", obj=cli)
+        m_cpt.return_value = out_cont
+
+        cli.main()
+
+        self.assertEqual(self.outputs, ["Input file name to process it: ",
+            "Do you want to encrypt or decrypt: ",
+            "Encrypting file somefile.txt ---","Done!"])
+
+        m_open.assert_called_with(infile,"r")
+        m_open.assert_called_with(outfile+".txt","wb")
+        i_f.read.assert_called()
+        m_gpw.getpasss.assert_called_with("Enter passfrase: ")
+        m_cpt.encrypt.assert_called_with(key,in_cont)
+        o_f.write.assert_called_with(out_cont)
+        o_f.close.assert_called()
+
     @skip("Not ready")
     def test_decrypting_ok():
         pass
+
     @skip("Not ready")
     def test_en_or_de_crypting_not_ok():
         pass
@@ -63,7 +100,7 @@ class TestMain(TestCase):
     def tearDown(self):
         self.inputs.clear()
         self.outputs.clear()
-
+        #patch.stopall()
 
     @classmethod
     def tearDownClass(self):
