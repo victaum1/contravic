505d53f3862e7e3180d336cb544d309195856ab2
Victor H. Manotas G <victorma31@gmail.com>
2019-09-16 16:50:01 -0500

    cli: testing decrypt fail

--- tests/test_contras_cli.py
+++ tests/test_contras_cli.py
@@ -89,9 +89,39 @@ class TestMain(TestCase):
         o_f.write.assert_called_with(out_cont)
         o_f.close.assert_called()
 
-    @skip("Not ready")
-    def test_decrypting_ok():
-        pass
+    @patch("builtins.input", side_effect = input_f)
+    @patch("builtins.print", side_effect = print_f)
+    def test_decrypting_ok(self, m_print, m_input):
+        infile   = "scrambled.txt"
+        outfile  = "out_put.txt"
+        key      = "some key!"
+        out_cont = "iiiiiiiiiiii"
+        in_cont  = "oooooooooooo"
+        i_f = Mock()
+        i_f.read.return_value = in_cont
+        o_f = Mock()
+        self.inputs.append(infile)
+        self.inputs.append("d")
+        m_open = self.create_patch("open", obj=cli)
+        m_open.side_efect = [i_f, o_f]
+        m_gpw = self.create_patch("gpw.getpass", obj=cli)
+        m_gpw.return_value = key
+        m_cpt = self.create_patch("cpt.decrypt", obj=cli)
+        m_cpt.return_value = out_cont
+
+        cli.main()
+
+        self.assertEqual(self.outputs, ["Input file name to process it: ",
+            "Do you want to encrypt or decrypt: ",
+            "Decrypting file scrambled.txt ---","Done!"])
+
+        m_open.assert_called_with(infile,"r")
+        m_open.assert_called_with(outfile,"wb")
+        i_f.read.assert_called()
+        m_gpw.getpasss.assert_called_with("Enter passfrase: ")
+        m_cpt.encrypt.assert_called_with(key,in_cont)
+        o_f.write.assert_called_with(out_cont)
+        o_f.close.assert_called()
 
     @skip("Not ready")
     def test_en_or_de_crypting_not_ok():
@@ -100,7 +130,6 @@ class TestMain(TestCase):
     def tearDown(self):
         self.inputs.clear()
         self.outputs.clear()
-        #patch.stopall()
 
     @classmethod
     def tearDownClass(self):
